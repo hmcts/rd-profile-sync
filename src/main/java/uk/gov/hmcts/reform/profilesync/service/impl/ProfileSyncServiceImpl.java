fdb9134c8525237f5bd172ee3267918162def9cd
Oozeerally <778425@AMBGB000004.local>
2019-10-10 11:55:52 +0100

    RDCC-542 fixing checkstyle...

--- src/main/java/uk/gov/hmcts/reform/profilesync/service/impl/ProfileSyncServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/profilesync/service/impl/ProfileSyncServiceImpl.java
@@ -44,8 +44,8 @@ public class ProfileSyncServiceImpl implements ProfileSyncService {
     @Autowired
     private final SyncJobRepository syncJobRepository;
 
-    final static String basic = "Basic ";
-    final static String bearer = "Bearer ";
+    static final String basic = "Basic ";
+    static final String bearer = "Bearer ";
 
     public String authorize() {
 
