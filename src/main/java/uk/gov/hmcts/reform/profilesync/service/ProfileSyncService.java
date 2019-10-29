11d416ad6e840e6912dc0782b34232045a647d72
jamesohmcts <james.odukoya@hmcts.net>
2019-10-29 12:46:59 +0000

    RDCC-529 Sonarcube updates minor issues

--- src/main/java/uk/gov/hmcts/reform/profilesync/service/ProfileSyncService.java
+++ src/main/java/uk/gov/hmcts/reform/profilesync/service/ProfileSyncService.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.reform.profilesync.service;
 import java.util.List;
 
 import uk.gov.hmcts.reform.profilesync.client.IdamClient;
-import uk.gov.hmcts.reform.profilesync.domain.UserProfileSyncException;
 
 public interface ProfileSyncService {
 
@@ -18,6 +17,6 @@ public interface ProfileSyncService {
 
     List<IdamClient.User> getSyncFeed(String bearerToken, String searchQuery);
 
-    void updateUserProfileFeed(String searchQuery) throws UserProfileSyncException;
+    void updateUserProfileFeed(String searchQuery);
 
 }
