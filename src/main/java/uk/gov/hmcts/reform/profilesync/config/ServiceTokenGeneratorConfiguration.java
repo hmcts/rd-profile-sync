d32701b8f8acdf8de69d8d7bfd05e3f2ad25656c
abhijit <Abhijit.diwan@hmcts.net>
2021-01-13 16:16:03 +0000

    test values

--- src/main/java/uk/gov/hmcts/reform/profilesync/config/ServiceTokenGeneratorConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/profilesync/config/ServiceTokenGeneratorConfiguration.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.profilesync.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +9,7 @@ import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGeneratorFactory;
 
 @Configuration
+@Slf4j
 public class ServiceTokenGeneratorConfiguration {
 
     @Bean
@@ -16,6 +18,8 @@ public class ServiceTokenGeneratorConfiguration {
             @Value("${idam.s2s-auth.microservice}") final String microService,
             final ServiceAuthorisationApi serviceAuthorisationApi
     ) {
+        log.info("::secret value:: {}",secret.substring(1,4));
+        log.info("::microService:: {}",microService);
         return AuthTokenGeneratorFactory.createDefaultGenerator(secret, microService, serviceAuthorisationApi);
     }
 
