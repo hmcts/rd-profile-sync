801c6aefc5e38ffa9a818b9a8778b7e97a2d6f45
Vijay Kumar Rajagopalan <vijay.rajagopalan@hmcts.net>
2021-01-21 15:01:30 +0000

    Delete unused secrets

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -9,33 +9,9 @@ data "azurerm_key_vault" "rd_key_vault" {
   resource_group_name = local.key_vault_name
 }
 
-resource "azurerm_key_vault_secret" "POSTGRES-USER" {
-  name          = join("-", [var.component, "POSTGRES-USER"])
-  value         = module.db-profile-sync-data.user_name
-  key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
-}
-
 resource "azurerm_key_vault_secret" "POSTGRES-PASS" {
   name          = join("-", [var.component, "POSTGRES-PASS"])
   value         = module.db-profile-sync-data.postgresql_password
-  key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
-}
-
-resource "azurerm_key_vault_secret" "POSTGRES_HOST" {
-  name          = join("-", [var.component, "POSTGRES-HOST"])
-  value         = module.db-profile-sync-data.host_name
-  key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
-}
-
-resource "azurerm_key_vault_secret" "POSTGRES_PORT" {
-  name          = join("-", [var.component, "POSTGRES-PORT"])
-  value         = "5432"
-  key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
-}
-
-resource "azurerm_key_vault_secret" "POSTGRES_DATABASE" {
-  name          = join("-", [var.component, "POSTGRES-DATABASE"])
-  value         = module.db-profile-sync-data.postgresql_database
   key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
 }
 
