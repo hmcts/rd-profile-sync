4a7b822749f3df65ba90c64275fb9009c891545b
rajkatla-hmcts <72351979+rajkatla-hmcts@users.noreply.github.com>
2021-08-23 11:58:21 +0100

    RDCC-3239- Fix Security scan issue

--- security.sh
+++ security.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 echo ${TEST_URL}
+echo "ZAP has successfully started"
 zap-api-scan.py -t ${TEST_URL}/v2/api-docs -f openapi -S -d -u ${SecurityRules} -P 1001 -l FAIL -J report.json -r api-report.html
 mkdir -p functional-output
 chmod a+wx functional-output
