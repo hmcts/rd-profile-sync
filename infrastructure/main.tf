0061c7d5ba0d23f8f89b755f2e3a35bfe18b84b2
Adil Cassam Oozeerally <49557838+AdilOoze@users.noreply.github.com>
2021-11-04 17:12:21 +0000

    Updating DB Secrets

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -30,19 +30,19 @@ resource "azurerm_key_vault_secret" "profile_sync_s2s_secret" {
 }
 resource "azurerm_key_vault_secret" "POSTGRES-USER" {
   name          = join("-", [var.component, "POSTGRES-USER"])
-  value         = module.db-profile-sync-data.user_name
+  value         = module.db-profile-sync-data-v11.user_name
   key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES-PASS" {
   name          = join("-", [var.component, "POSTGRES-PASS"])
-  value         = module.db-profile-sync-data.postgresql_password
+  value         = module.db-profile-sync-data-v11.postgresql_password
   key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES_HOST" {
   name          = join("-", [var.component, "POSTGRES-HOST"])
-  value         = module.db-profile-sync-data.host_name
+  value         = module.db-profile-sync-data-v11.host_name
   key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
 }
 
@@ -54,7 +54,7 @@ resource "azurerm_key_vault_secret" "POSTGRES_PORT" {
 
 resource "azurerm_key_vault_secret" "POSTGRES_DATABASE" {
   name          = join("-", [var.component, "POSTGRES-DATABASE"])
-  value         = module.db-profile-sync-data.postgresql_database
+  value         = module.db-profile-sync-data-v11.postgresql_database
   key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
 }
 
