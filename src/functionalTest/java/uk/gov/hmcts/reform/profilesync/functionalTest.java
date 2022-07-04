536ba4eee1d883ea4258ad99bb6f6f2cfa977656
Sahitya Desireddy <sahitya.desireddy@hmcts.net>
2022-07-04 16:02:55 +0100

    CVE FIX

---
+++ src/functionalTest/java/uk/gov/hmcts/reform/profilesync/functionalTest.java
@@ -0,0 +1,12 @@
+package uk.gov.hmcts.reform.profilesync;
+
+import org.junit.jupiter.api.Test;
+
+public class functionalTest {
+
+    @Test
+    void testExternalUserScenario() {
+        System.out.println("Test");
+    }
+
+}
