f949e2bcfe7e3631c157600268292e4ceb136665
SujeethKumarAri <73644938+SujeethKumarAri@users.noreply.github.com>
2022-07-06 14:55:52 +0100

    fix: added functional test

---
+++ src/functionalTest/java/uk/gov/hmcts/reform/profilesync/SyncFunctionalTest.java
@@ -0,0 +1,21 @@
+package uk.gov.hmcts.reform.profilesync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import uk.gov.hmcts.reform.profilesync.domain.SyncJobConfig;
+import uk.gov.hmcts.reform.profilesync.repository.SyncConfigRepository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class SyncFunctionalTest {
+    @Autowired
+    SyncConfigRepository syncConfigRepository;
+    @Test
+    void shouldReturnConfigRun() {
+        SyncJobConfig syncJobConfig = syncConfigRepository.findByConfigName("firstsearchquery");
+        assertEquals("72h", syncJobConfig.getConfigRun());
+
+    }
+}
