f26b3e900ba9dc9345856d600416da4a61d6f2b0
SabinaHMCTS <sabina.sharangdhar@hmcts.net>
2023-11-14 13:03:57 +0000

    DTSRD-1466.Updating schema parameter with hibernate

--- src/test/java/uk/gov/hmcts/reform/profilesync/repository/ProfileSyncAuditRepositoryTest.java
+++ src/test/java/uk/gov/hmcts/reform/profilesync/repository/ProfileSyncAuditRepositoryTest.java
@@ -2,12 +2,9 @@ package uk.gov.hmcts.reform.profilesync.repository;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 import uk.gov.hmcts.reform.profilesync.domain.ProfileSyncAudit;
 
