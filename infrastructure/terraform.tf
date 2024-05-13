b591f0ebfa69f1729f3b7bdaa8b6fcde116a987c
SabinaHMCTS <139119493+SabinaHMCTS@users.noreply.github.com>
2024-05-13 19:30:37 +0100

    Update terraform.tf

--- infrastructure/terraform.tf
+++ infrastructure/terraform.tf
@@ -1,8 +1,5 @@
 provider "azurerm" {
   features {
-    resource_group {
-      prevent_deletion_if_contains_resources = false
-    }
   }
   skip_provider_registration = true
   alias                      = "postgres_network"
