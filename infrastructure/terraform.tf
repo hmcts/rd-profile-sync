a84009b68435b507c92d4c08c312c066e7fa5aa5
SabinaHMCTS <139119493+SabinaHMCTS@users.noreply.github.com>
2024-05-13 18:47:19 +0100

    Dtspo 16369 (#844)
    * Update schema ownership and force permission
    
    * Update schema ownership and force permission
    
    * Update schema ownership and force permission
    
    * Update schema ownership and force permission
    
    * Delete single server
    
    * Delete single server
    
    * Delete single server

--- infrastructure/terraform.tf
+++ infrastructure/terraform.tf
@@ -1,12 +1,4 @@
 provider "azurerm" {
-  features {
-    resource_group {
-      prevent_deletion_if_contains_resources = false
-    }
-  }
-}
-
-provider "azurerm" {
   features {}
   skip_provider_registration = true
   alias                      = "postgres_network"
