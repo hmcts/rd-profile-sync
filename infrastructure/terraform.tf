364a2fd85a593fcbc9670e9c830b0a4f260798d7
SabinaHMCTS <sabina.sharangdhar@hmcts.net>
2024-05-14 11:48:45 +0100

    Delete single server

--- infrastructure/terraform.tf
+++ infrastructure/terraform.tf
@@ -4,6 +4,7 @@ provider "azurerm" {
       prevent_deletion_if_contains_resources = false
     }
   }
+
   skip_provider_registration = true
   alias                      = "postgres_network"
   subscription_id            = var.aks_subscription_id
