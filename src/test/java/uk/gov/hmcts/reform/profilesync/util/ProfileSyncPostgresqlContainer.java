5761a138659c5631f408cb69df471c47ec8969d1
lukasz-wolski <1005015+lukasz-wolski@users.noreply.github.com>
2024-08-22 17:42:15 +0100

    cleanup

--- src/test/java/uk/gov/hmcts/reform/profilesync/util/ProfileSyncPostgresqlContainer.java
+++ src/test/java/uk/gov/hmcts/reform/profilesync/util/ProfileSyncPostgresqlContainer.java
@@ -1,17 +1,10 @@
 package uk.gov.hmcts.reform.profilesync.util;
 
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-import javax.sql.DataSource;
-
-//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.AUTO_CONFIGURED)
 @Testcontainers
-//@Configuration
 public class ProfileSyncPostgresqlContainer extends PostgreSQLContainer<ProfileSyncPostgresqlContainer> {
     private static final String IMAGE_VERSION = "postgres:11.1";
 
@@ -21,10 +14,5 @@ public class ProfileSyncPostgresqlContainer extends PostgreSQLContainer<ProfileS
 
     @Container
     private static final ProfileSyncPostgresqlContainer container = new ProfileSyncPostgresqlContainer();
-
-//    @Bean
-//    public DataSource dataSource() {
-//        return container.
-//    }
 
 }
